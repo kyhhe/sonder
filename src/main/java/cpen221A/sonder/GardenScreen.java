3de579f777f29860b7c84e783ab20deaedc96818
cbella06 <cbella0627@gmail.com>
2025-10-30 11:13:58 -0700

    added spec of garden screen

--- src/main/java/cpen221A/sonder/GardenScreen.java
+++ src/main/java/cpen221A/sonder/GardenScreen.java
@@ -40,4 +40,59 @@ public class GardenScreen extends AbstractScreen{
 
         return pane;
     }
+
+    /**
+     * Get the flower selected by user of the day.
+     * @return the flower sequence
+     */
+    public String getFlower (){
+        return StartScreen.getFlower(); //get from the Start Screen!!!
+    }
+
+    /**
+     * Add a flower to the garden.
+     *
+     * @param flowerName the name/type of the flower
+     * @param positionX the row of the position selected(0-2), position should not have flower previously
+     * @param positionY the column of the position selected(0-4), position should not have flower previously
+     * @return true if added flower to the garden, false otherwise
+     * @throws IllegalArgumentException if position is invalid
+     */
+    public boolean addFlower (String flowerName, int positionX, int positionY){
+        return false;
+    }
+
+    /**
+     * Checks if the position already has a flower
+     *
+     * @param x row index
+     * @param y column index
+     * @return true if the position is empty, false otherwise
+     */
+    public boolean hasFlower(int x, int y) {
+        return false;
+    }
+
+    /**
+     * Checks if the garden is full (reaches the 15th day)
+     *
+     * @return true if all 15 cells have flowers
+     */
+    public boolean isFull() {
+        return false;
+    }
+
+    /**
+     * Saves the current garden state
+     */
+    public void saveGarden(){
+
+    }
+
+    /**
+     * Loads the garden state
+     */
+    public void loadGarden(){
+
+    }
 }
