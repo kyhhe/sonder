9ca3886083883dfeb13c7a1d22d4e95a7f385c7a
Anna Jian <annajian0813@gmail.com>
2025-10-30 20:19:46 -0700

    Created folders for respective images

--- src/main/java/cpen221A/sonder/GeneralTasks.java
+++ src/main/java/cpen221A/sonder/GeneralTasks.java
@@ -1,4 +1,33 @@
 package cpen221A.sonder;
 
 public interface GeneralTasks {
+
+    /**
+     * Returns current task completion status
+     *
+     * @return true iif user has completed the current task (ie. valid input), false otherwise
+     */
+    public boolean currentComplete();
+
+    /**
+     * Determines if user has pressed ENTER to move onto next task
+     *
+     * @return true if the user has pressed ENTER, false otherwise
+     */
+    public boolean inputEnter();
+
+    /**
+     * Displays a warning message on the task screen if no valid user input and user pressed ENTER
+     *
+     * @param inputEnter if user has pressed ENTER
+     */
+    public void warningMessage(boolean isComplete, boolean inputEnter);
+
+
+    /**
+     * Returns total task completion status
+     *
+     * @return true if all tasks are complete, false otherwise
+     */
+    public boolean allComplete();
 }
--- src/main/java/cpen221A/sonder/StartScreen.java
+++ src/main/java/cpen221A/sonder/StartScreen.java
@@ -35,4 +35,6 @@ public class StartScreen extends AbstractScreen{
 
         return pane;
     }
+
+
 }
