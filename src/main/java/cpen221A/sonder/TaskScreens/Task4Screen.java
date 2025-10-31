a112085a45f177420cd50b3c1faab4283c639d96
Anna Jian <annajian0813@gmail.com>
2025-10-31 00:13:50 -0700

    Updated Task4Screen

--- src/main/java/cpen221A/sonder/TaskScreens/Task4Screen.java
+++ src/main/java/cpen221A/sonder/TaskScreens/Task4Screen.java
@@ -15,6 +15,9 @@ public class Task4Screen extends AbstractScreen implements GeneralTasks {
     public Task4Screen(MainApplication main){
         super(main);
     }
+
+    private Label warning;
+
     @Override
     protected Parent createRoot() {
         // Initialize Screen elements
@@ -24,21 +27,53 @@ public class Task4Screen extends AbstractScreen implements GeneralTasks {
         StackPane pane = new StackPane();
 
         // Screen elements
-        Label title = createTitle("redeem your reward:");
+        Label title = createTitle("reward:");
+        Label question = createText("choose a flower for your garden.");
         Button backButton = createButton("menu");
+
+        warning = createText("please select your flower of choice.");
+        warning.setVisible(false);
 
         // Button actions
         backButton.setOnAction(e -> main.showMainMenu());
 
         // Add elements to screen
-        vbox.getChildren().addAll(title, backButton);
+        vbox.getChildren().addAll(title, question, warning, backButton);
         vbox.setAlignment(javafx.geometry.Pos.CENTER);
         pane.getChildren().add(vbox);
 
         return pane;
     }
 
-    public String getFlower() {
+    /**
+     * Gets input from user
+     * (if user does not input a valid answer and presses enter, calls on warningMessage)
+     * (otherwise, returns String input)
+     * @return String representing user's short answer response to task 4
+     */
+    public String getTask4Input() {
         return null;
+    }
+
+    /**
+     * Checks and returns task completion status
+     *
+     * @return true if user completes task, false otherwise
+     */
+    public boolean currentComplete() {
+        return false;
+    }
+
+    /**
+     * Displays a custom warning message on the task screen
+     */
+    public void warningMessage() {
+        warning.setVisible(true);
+    }
+
+    /**
+     * Transitions to next task screen if current task is completed
+     */
+    public void nextTask() {
     }
 }
