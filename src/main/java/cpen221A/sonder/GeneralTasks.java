9f0fe29c5a7edf812c81ac346203cf8dde846cac
Anna Jian <annajian0813@gmail.com>
2025-10-31 00:09:58 -0700

    Refactor GeneralTasks interface methods

--- src/main/java/cpen221A/sonder/GeneralTasks.java
+++ src/main/java/cpen221A/sonder/GeneralTasks.java
@@ -4,22 +4,16 @@ public interface GeneralTasks {
 
     /**
      * Returns current task completion status
-     *
-     * @return true iif user has completed the current task (i.e. valid input), false otherwise
      */
     boolean currentComplete();
 
     /**
-     * Determines if user has pressed ENTER to move onto next task
-     *
-     * @return true if the user has pressed ENTER, false otherwise
+     * Displays a warning message on the task screen
      */
-    boolean inputEnter();
+    void warningMessage();
 
     /**
-     * Displays a warning message on the task screen if no valid user input and user pressed ENTER
-     *
-     * @param inputEnter if user has pressed ENTER
+     * Transitions to next task screen if current task is completed
      */
-    void warningMessage(boolean isComplete, boolean inputEnter);
+    void nextTask();
 }
