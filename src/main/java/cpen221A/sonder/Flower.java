ad4876a63139e378f1ea012b7216dfce9e0a51bc
Kelly <hkellyhe@gmail.com>
2025-10-31 11:51:35 -0700

    Added new Flower class to keep track of flowers in the Garden and in the Entries

---
+++ src/main/java/cpen221A/sonder/Flower.java
@@ -0,0 +1,19 @@
+package cpen221A.sonder;
+
+import java.awt.Image;
+
+/**
+ * Class containing methods for creating, modifying and representing flowers. Each flower has a unique
+ * ID, with each ID corresponding to a respective image.
+ */
+public class Flower {
+    private int flowerId;
+    private Image flowerImage;
+
+    /**
+     * Creates a new flower object
+     * @param id Unique ID to represent the flower object and its corresponding image
+     */
+    public Flower(int id) {
+    }
+}
