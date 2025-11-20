ee14e8e066848c764bc50fc5dd622d09c0db41c0
Anna Jian <annajian0813@gmail.com>
2025-11-19 17:36:53 -0800

    Implemented task 3 (WIP)

--- src/main/java/cpen221A/sonder/TaskScreens/Task1Screen.java
+++ src/main/java/cpen221A/sonder/TaskScreens/Task1Screen.java
@@ -4,19 +4,26 @@ import cpen221A.sonder.AbstractScreen;
 import cpen221A.sonder.GeneralTasks;
 import cpen221A.sonder.MainApplication;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * Creates the screen which provides the user interface for Task 1: How are you feeling today?
  */
 public class Task1Screen extends AbstractScreen implements GeneralTasks {
+
     private String answer;
+    public String input = "";
     private Label warning;
     private Runnable onComplete;
+    private boolean toNext = false;
 
     public Task1Screen(MainApplication main){
         super(main);
@@ -36,19 +43,59 @@ public class Task1Screen extends AbstractScreen implements GeneralTasks {
         Label question = createText("how are you feeling today?");
         Button nextButton = createButton("next");
 
+        // Image selection elements
+        HBox rowFaces = new HBox(25);
+        rowFaces.setAlignment(Pos.CENTER);
+
+        String[] faces = {
+                "/FaceImages/Face1.PNG",
+                "/FaceImages/Face2.PNG",
+                "/FaceImages/Face3.PNG",
+                "/FaceImages/Face4.PNG",
+                "/FaceImages/Face5.PNG"
+        };
+
+        for(String face : faces) {
+            Image img = new Image(getClass().getResource(face).toExternalForm());
+            ImageView faceView = new ImageView(img);
+            faceView.setFitWidth(150);
+            faceView.setPreserveRatio(true);
+
+            Button faceButton = new Button();
+            faceButton.setGraphic(faceView);
+            faceButton.setStyle("-fx-background-color: transparent; -fx-padding: 5;");
+
+            faceButton.setOnAction(e -> {
+                this.input = face;
+                // add code for visible confirmation that button was selected
+            });
+            rowFaces.getChildren().add(faceButton);
+        }
 
         // warning message
         warning = createText("please select one of the faces.");
         warning.setVisible(false);
 
         // Button action and task transition logic
-        nextButton.setOnAction(e ->  {if (onComplete != null) {
-            onComplete.run();};
+        nextButton.setOnAction(e -> {
+            if (this.input == null || this.input.isEmpty()) {
+                this.warning.setVisible(true);
+            }
+            else {
+                this.warning.setVisible(false);
+                this.answer = this.input;
+
+                if (this.onComplete != null) {
+                    this.toNext = true;
+                    this.onComplete.run();
+                }
+            }
         });
 
         // Add elements to screen
-        vbox.getChildren().addAll(title, question, this.warning, nextButton);
-        vbox.setAlignment(javafx.geometry.Pos.CENTER);
+        vbox.getChildren().addAll(title, question, rowFaces, this.warning, nextButton);
+        VBox.setMargin(rowFaces, new Insets(20, 0, 20, 0));
+        vbox.setAlignment(Pos.CENTER);
         pane.getChildren().add(vbox);
 
         return pane;
@@ -70,14 +117,7 @@ public class Task1Screen extends AbstractScreen implements GeneralTasks {
      * @return true if user completes task, false otherwise
      */
     public boolean currentComplete() {
-        return false;
-    }
-
-    /**
-     * Displays a custom warning message on the task screen
-     */
-    public void warningMessage() {
-        warning.setVisible(true);
+        return this.toNext;
     }
 
     /**
